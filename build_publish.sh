a3c8bf0b37f30b962f270a7b75d13bf9d5c352ec
Han Wu <xjohnwu@gmail.com>
2021-03-27 17:25:58 +0800

    update

--- build_publish.sh
+++ build_publish.sh
@@ -5,8 +5,8 @@ my_dir="$(dirname "$0")"
 pushd "$my_dir"
 rm -rf ./*.egg-info build dist
 source ./venv/bin/activate
-python3 -m pip install --upgrade setuptools wheel twine
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload --repository-url http://nexus.cryptopian.io:8081/repository/pypi-hosted/ dist/*
+python -m pip install --upgrade setuptools wheel twine
+python setup.py sdist bdist_wheel
+python -m twine upload --repository-url http://nexus.cryptopian.io:8081/repository/pypi-hosted/ dist/*
 rm -rf spool_api.egg-info build dist
 popd
--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from setuptools import setup
 
 setup(
@@ -17,12 +16,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
