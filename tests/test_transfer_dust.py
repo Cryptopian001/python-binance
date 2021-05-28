5255ca53ca6c686b29746a9b1849ed86e081164c
Han Wu <xjohnwu@gmail.com>
2021-05-28 11:43:59 +0800

    update

--- tests/test_transfer_dust.py
+++ tests/test_transfer_dust.py
@@ -14,6 +14,9 @@ class TestMargin:
         secret = load_json_config(os.path.join(os.path.dirname(__file__), 'secret.json'))
         self.client = Client(**secret['fanyong'])
 
-    def test_transfer_dust(self):
-        res = self.client.transfer_dust(asset=['BCH', 'BTC'])
-        print(res)
+    # def test_transfer_dust(self):
+    #     res = self.client.transfer_dust(asset=['BCH', 'BTC'])
+    #     print(res)
+
+    def test_dust(self):
+        print(self.client.get_dust_log())
